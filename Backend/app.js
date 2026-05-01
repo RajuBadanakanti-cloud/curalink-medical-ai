43040a45d9cf8e0f8c96e7e903f89480c9f454cc
Raju Badanakanti <rajubadanakanti7@gmail.com>
2026-05-01 20:22:58 +0530

    origin updated

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,9 @@ import queryRoutes from "./routes/queryRoutes.js"
 
 const app = express()
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin:[process.env.FRONTEND_URL, "http://localhost:5173"]
+}))
 
 
 connectDB() // db connected
